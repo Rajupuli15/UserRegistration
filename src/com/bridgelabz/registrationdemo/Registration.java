b702b56b7c1dd3ebbe10eb216b095745bd54b424
Rajupuli15 <Rajupuli15@gmail.com>
2021-09-30 07:56:42 -0400

    Added:Welcome

---
+++ src/com/bridgelabz/registrationdemo/Registration.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.registrationdemo;
+
+public class Registration {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome for Registration");
+    }
+}
